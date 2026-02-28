35bd4c52c98b9c5d4e11373e8f8f124342b07acf
xch <xchtzon@gmail.com>
2026-02-28 23:45:20 +0800

    learn condition

---
+++ cmd/learn/condition.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := 1, 2
+
+	if a == b {
+		fmt.Println("a == b")
+	} else {
+		fmt.Println("a != b")
+	}
+
+	if x := 1; x > 1 {
+		fmt.Println(x)
+	} else {
+		fmt.Println("错误的")
+	}
+
+	score := 80
+
+	if score > 90 {
+		fmt.Println("优秀")
+	} else if score >= 75 {
+		fmt.Println("良好")
+	} else if score >= 60 {
+		fmt.Println("及格")
+	} else {
+		fmt.Println("不及格")
+	}
+
+	switch_val := "b"
+
+	switch switch_val {
+	case "a":
+		fmt.Println("是a")
+	case "b":
+		fmt.Println("是b")
+		fallthrough
+	case "bc":
+		fmt.Println("包含b")
+		fallthrough
+	case "c":
+		fmt.Println(switch_val)
+	}
+
+}
