daf0a179b0bf6897d925a79e1d1be355b928e1d2
xch <axchtzon@gmail.com>
2026-03-16 23:59:47 +0800

    learn string-2

--- cmd/learn/string.go
+++ cmd/learn/string.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println('a')
+	fmt.Print(`
+		343243243
+		ewrsedrfdsf
+		fdsfd
+	`)
+	a := "abcdefg "
+	fmt.Println(a[1])
+	fmt.Println(string(a[1]))
+	fmt.Println(a[:2])
+	fmt.Println(string(a[:2]))
+	fmt.Println(string(a))
 }
