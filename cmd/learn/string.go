4ea4e63d5afe1f3b989fe29fd547b850a952b647
xch <axchtzon@gmail.com>
2026-03-20 00:10:26 +0800

    learn string-4

--- cmd/learn/string.go
+++ cmd/learn/string.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println('a')
@@ -20,7 +23,47 @@ func main() {
 	fmt.Println(bytes)
 	fmt.Println(string(bytes))
 
-	bytes = append(bytes, 109, 103)
+	bytes = append(bytes, 109, 103, 32, 111)
+
+	bytes[0] = 120
 	fmt.Println(string(bytes))
 
+	b := "一二三四五"
+
+	fmt.Println(len(b))
+
+	copB := make([]byte, 111)
+
+	copy(copB, b)
+
+	cB := strings.Clone(b)
+
+	fmt.Println(string(copB))
+	fmt.Println(string(cB))
+
+	pB := b + "你好"
+	fmt.Println(pB)
+	pBb := []byte(pB)
+	pBb = append(pBb, "nihaoa"...)
+	fmt.Println(string(pBb))
+
+	c := string(pBb)
+	c += "13213232"
+	fmt.Println(c)
+
+	d := "123456"
+	fmt.Println(d[1])
+	db := []byte(d)
+	fmt.Println(db[1])
+	fmt.Println(d[1] == db[1])
+
+	额 := "中国"
+	e := []rune(额)
+	fmt.Println(string(e[1]))
+
+	builder := strings.Builder{}
+
+	builder.WriteString("链接B")
+	builder.WriteString("链接C")
+	fmt.Println(builder.String())
 }
