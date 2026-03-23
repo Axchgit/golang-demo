7c665a318268b99c3bea156be27ee55c3e68f494
xch <axchtzon@gmail.com>
2026-03-24 01:51:19 +0800

    learn map-2

--- cmd/learn/map.go
+++ cmd/learn/map.go
@@ -11,6 +11,23 @@ func main() {
 		2: "b",
 	}
 
+	a := map[int]string{
+		1: "123",
+	}
+
+	a[1] = "change"
+
+	fmt.Println(a)
+
+	aa := make(map[string]int, 10)
+	aa["a"] = 123
+	fmt.Println(aa)
+
+	// 错误，引用类型，没有分配内存  panic: assignment to entry in nil map
+	// var ab map[string]int
+	// ab["123"] = 123
+	// fmt.Println(ab)
+
 	for index, value := range mp {
 		fmt.Println(index, value)
 	}
