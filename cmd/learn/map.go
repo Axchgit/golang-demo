112a4f3bd55b49bd2bbfa123a175c6207ff9b15b
xingchenhao <axchtzon@gmail.com>
2026-03-25 20:28:05 +0800

    learn map-3

--- cmd/learn/map.go
+++ cmd/learn/map.go
@@ -33,5 +33,28 @@ func main() {
 	}
 
 	fmt.Println(mp)
+	fmt.Println(aa["111"])
 
+	ab := make(map[int]string, 10)
+	ab[1] = "123"
+	fmt.Println(ab)
+
+	if _, exit := ab[11]; exit {
+		fmt.Println("11 exists")
+	} else {
+		fmt.Println(11)
+	}
+
+	delete(ab, 1)
+
+	fmt.Println(ab)
+
+	for i := 0; i < 10; i++ {
+		ab[i] = "nihao:" + fmt.Sprintf("%d", i)
+	}
+
+	fmt.Println(ab[1])
+	clear(ab)
+	fmt.Println(ab[1])
+	fmt.Println(ab)
 }
